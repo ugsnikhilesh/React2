643a3f171f65a45838843e56fe54a140d554c3ca
ugsnikhilesh <gsnikhilesh.utukuri@gmail.com>
2023-06-21 11:43:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Children, useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [expressions, UpdateExpList] = useState([]);
+  const [curr_exp, setExp] = useState('');
+
+  const clearExp = () => {
+    setExp('');
+  }
+
+  const evalExp = () => {
+    var abcd;
+    try {
+      abcd = eval(curr_exp);
+      abcd = curr_exp + " = " + abcd;
+      document.getElementById("ans").innerHTML = abcd;
+      UpdateExpList((oldExp) => [...oldExp, abcd]);
+    } catch (error) {
+      alert("Enter valid expression!")
+    }
+    clearExp();
+  }
+
+  const handleClick = (e) => {
+    console.log(curr_exp + e.target.innerText);
+    setExp(curr_exp + e.target.innerText);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div style={{ textAlign: "center", paddingTop: "30px"}}><h2 style={{fontWeight:"bold",color:"red"}}>My Calculator</h2></div>
+      <div id='abcd'>
+        <div className="outer">
+          <div className="inner">
+            <table>
+              <tr>
+                <td colSpan={4} style={{ backgroundColor: "black", color: "white" }} id='ans'></td>
+              </tr>
+              <tr>
+                <td id="clear" onClick={clearExp}>C</td>
+                <td colSpan="3" id="screen">{curr_exp}</td>
+              </tr>
+              <tr>
+                <td onClick={handleClick}>7</td>
+                <td onClick={handleClick}>8</td>
+                <td onClick={handleClick}>9</td>
+                <td onClick={handleClick}>/</td>
+              </tr>
+              <tr>
+                <td onClick={handleClick}>4</td>
+                <td onClick={handleClick}>5</td>
+                <td onClick={handleClick}>6</td>
+                <td onClick={handleClick}>*</td>
+              </tr>
+              <tr>
+                <td onClick={handleClick}>1</td>
+                <td onClick={handleClick}>2</td>
+                <td onClick={handleClick}>3</td>
+                <td onClick={handleClick}>-</td>
+              </tr>
+              <tr>
+                <td onClick={handleClick}>0</td>
+                <td onClick={handleClick}>.</td>
+                <td onClick={evalExp}>=</td>
+                <td onClick={handleClick}>+</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
